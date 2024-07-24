e1ba709f1113a061ce9e88c11d0f3c814dadecdf
Kaho Shibuya <kshibuya31@gmail.com>
2024-07-23 18:26:16 -0700

    add style for disable state

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -9,7 +9,7 @@ export function Button({ children, className, ...rest }: ButtonProps) {
 		<button
 			{...rest}
 			className={clsx(
-				"text-center font-serif border border-solid border-black bg-black text-white w-32 text-base bold sm:text-lg",
+				"text-center font-serif border border-solid border-black bg-black text-white w-32 text-base bold sm:text-lg disabled:bg-slate-300 disabled:border-slate-300 disabled:text-slate-900",
 				className
 			)}
 		>
