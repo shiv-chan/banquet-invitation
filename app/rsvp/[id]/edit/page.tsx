2e1ba3ab5cd522a1f7b1dc879166de7d803ae84b
Kaho Shibuya <kshibuya31@gmail.com>
2024-07-24 17:33:15 -0700

    make edit confirm page dynamic

--- app/rsvp/[id]/edit/page.tsx
+++ app/rsvp/[id]/edit/page.tsx
@@ -1,19 +1,23 @@
 import { merriweather } from "@/app/ui/fonts";
 import Link from "next/link";
 
-export default function Page() {
+export default function Page({ params }: { params: { id: string } }) {
+	const id = params.id;
+
 	return (
 		<div className='font-serif text-center'>
 			<div className='flex flex-col justify-center h-60'>
-				<p
+				<h2
 					className={`${merriweather.className} italic font-bold mb-4 text-xl lg:text-2xl`}
 				>
 					It seems you have already RSVP’d!
+				</h2>
+				<p className='font-serif mb-1 block text-sm sm:text-base'>
+					Do you want to edit it?
 				</p>
-				<p>Do you want to edit it?</p>
 			</div>
 			<Link
-				href='/rsvp/1'
+				href={`/rsvp/${id}`}
 				className='block mt-4 text-center font-serif border border-solid border-black bg-black text-white w-full text-base bold sm:text-lg'
 			>
 				Yup
