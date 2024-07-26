92968acfb445fcd86487dc48b6c75b6ccdf4197f
Kaho Shibuya <kshibuya31@gmail.com>
2024-07-25 22:38:53 -0700

    add viewport object

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import "@/app/ui/global.css";
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 
 export const metadata: Metadata = {
 	title: "The Wedding Celebration of Kaho + Jade",
 	description: "The Wedding Cerebration of Kaho and Jade",
+};
+
+export const viewport: Viewport = {
+	width: "device-width",
+	interactiveWidget: "overlays-content",
 };
 
 export default function RootLayout({
