dc1747a8ba432a6eb725818323e7d0d866f42071
Kaho Shibuya <kshibuya31@gmail.com>
2024-07-30 23:22:11 -0700

    add confetti

--- app/rsvp/thank-you/page.tsx
+++ app/rsvp/thank-you/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import Link from "next/link";
 import { merriweather } from "@/app/ui/fonts";
+import confetti from "canvas-confetti";
+import { useLayoutEffect } from "react";
 
 export default function Page() {
+	useLayoutEffect(() => {
+		const heart = confetti.shapeFromPath({
+			path: "m 29.4,121.7 c -0.9,-79.8 66.4,-48.1 77,-14 6.6,-34.7 82.2,-66.6 81.7,8.9 -0.3,44.9 -77.5,101.9 -77.5,101.9 0,0 -80.7,-49.2 -81.3,-96.8 z",
+		});
+
+		confetti({
+			particleCount: 100,
+			spread: 100,
+			shapes: [heart],
+			scalar: 2,
+		});
+	}, []);
+
 	return (
 		<div className='font-serif text-center'>
 			<div className='flex flex-col justify-center h-60'>
