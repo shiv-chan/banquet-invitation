64ce6db179c47a807ed0961e5e1179b54fea274c
Kaho Shibuya <kshibuya31@gmail.com>
2026-07-02 17:25:00 -0700

    fix type errors

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,22 +1,25 @@
-import { neon, FullQueryResults} from '@neondatabase/serverless';
+import { FullQueryResults, neon } from '@neondatabase/serverless';
+import { Guest } from "@/app/lib/definitions";
+
 const sql = neon(<string>process.env.DATABASE_URL,  { fullResults: true });
 
-export async function fetchGuest(id: string) {
+export async function fetchGuest(id: string): Promise<Guest> {
+	let data: FullQueryResults<false>;
 	try {
-		const data: FullQueryResults<false> = await sql`
+		data = await sql`
             SELECT *
             FROM guests
             WHERE id = ${id};
         `;
 
-		return data.rows[0];
+		return data.rows[0] as Guest;
 	} catch (e) {
 		console.error("Database Error:", e);
 		throw new Error("Failed to fetch data of a guest");
 	}
 }
 
-export async function fetchCompanies(id: string, group_id: number) {
+export async function fetchCompanies(id: string, group_id: number): Promise<Guest[]> {
 	try {
 		const data: FullQueryResults<false> = await sql`
             SELECT *
@@ -25,7 +28,7 @@ export async function fetchCompanies(id: string, group_id: number) {
             ORDER BY self_submitted, first, last ASC;
         `;
 
-		return data.rows;
+		return data.rows as Guest[];
 	} catch (e) {
 		console.error("Database Error:", e);
 		throw new Error("Failed to fetch guest's companies");
